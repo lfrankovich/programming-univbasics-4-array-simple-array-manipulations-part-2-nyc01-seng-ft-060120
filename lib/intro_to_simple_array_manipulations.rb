e1471ead66065ca2360187b83514ad0a4199b42a
Lauren Frankovich <frankovister@gmail.com>
2020-05-24 03:25:05 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_concat(my_favorite_things, more_favs)
+  my_favorite_things.concat(more_favs)
+end
+
+def using_insert(new_array, another_language)
+  new_array.insert(4, another_language)
+end
+
+def using_uniq(haircuts)
+  haircuts.uniq
+end 
+
+def using_flatten(instruments)
+  instruments.flatten 
+end 
+
+def using_delete(instructors,string)
+  instructors.delete(string)
+end
+
+def using_delete_at(famous_robots, integer)
+  famous_robots.delete_at(2)
+end 
