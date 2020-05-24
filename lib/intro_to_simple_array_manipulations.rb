2a5ecbefc778bd0c2a01876fff4c834101db997a
Lauren Frankovich <frankovister@gmail.com>
2020-05-24 03:22:38 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,19 @@
+def using_concat(my_favorite_things, more_favs)
+  my_favorite_things.concat(more_favs)
+end
+
+def using_insert(new_array, another_language)
+  new_array.insert(4, another_language)
+end
+
+def using_uniq(haircuts)
+  haircuts.uniq
+end 
+
+def using_flatten(instruments)
+  instruments.flatten 
+end 
+
+def using_delete(instructors,"Steven")
+  instructors.delete("Steven")
+end
